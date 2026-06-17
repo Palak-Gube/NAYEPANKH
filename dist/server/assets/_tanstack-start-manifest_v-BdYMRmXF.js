ee98301ffcd81a95f8388299ac5a71a183af0eca
Palak-Gube <palakgube@gmail.com>
2026-06-17 20:01:50 +0530

    .

---
+++ dist/server/assets/_tanstack-start-manifest_v-BdYMRmXF.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/asus/OneDrive/Desktop/NayePankh/src/routes/__root.tsx", children: ["/", "/programs"], preloads: ["/assets/index-CA28fOCF.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CA28fOCF.js" } }] }, "/": { filePath: "C:/Users/asus/OneDrive/Desktop/NayePankh/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CGmuhKXQ.js", "/assets/proxy-CM6XR6ie.js"] }, "/programs": { filePath: "C:/Users/asus/OneDrive/Desktop/NayePankh/src/routes/programs.tsx", children: void 0, preloads: ["/assets/programs-jthAT8F4.js", "/assets/proxy-CM6XR6ie.js"] } } });
+export {
+  tsrStartManifest
+};
--- dist/server/server.js
+++ dist/server/server.js
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./assets/server-2FZptYFg.js").then(
+    serverEntryPromise = import("./assets/server-BzNt7kUm.js").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
